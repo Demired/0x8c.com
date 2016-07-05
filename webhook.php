55e29dac66ee707e3667b6068d078b1a0581ca72
0x8c <nevermakyoucrytoli@gmail.com>
2016-07-05 22:21:36 +0800

    all

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@ require(__DIR__ . "/vendor/autoload.php");
 
 use GitHubWebhook\Handler;
 
-$handler = new Handler("123456", __DIR__);
+$handler = new Handler("123456", __DIR__,"origin master");
 if($handler->handle()) {
     file_put_contents('true','ok');
 } else {
